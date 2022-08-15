48f59cd29adb334cb9588c304e187504a3305f2d
pjsjongsung <pjsjongsung@gmail.com>
2022-08-15 00:12:35 -0400

    fixed issues

--- dipy/workflows/tests/test_io.py
+++ dipy/workflows/tests/test_io.py
@@ -63,6 +63,7 @@ def test_io_fetch_fetcher_datanames():
                      'stanford_labels', 'stanford_pve_maps', 'stanford_t1',
                      'syn_data', 'taiwan_ntu_dsi', 'target_tractogram_hcp',
                      'tissue_data', 'qte_lte_pte', 'resdnn_weights',
+                     'synb0_weights', 'synb0_test',
                      'DiB_217_lte_pte_ste', 'DiB_70_lte_pte_ste']
 
     num_expected_fetch_methods = len(dataset_names)
